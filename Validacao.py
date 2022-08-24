0300d979d1afcc2f6d09bcc55d2f96593a25b38b
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-08-24 14:23:15 -0300

    Update Validacao.py

--- Validacao.py
+++ Validacao.py
@@ -1,13 +1,18 @@
 def validacao(**kwargs):
     if kwargs.get("tipo")!=None:
         mensagem =kwargs.get("mensagem") if kwargs.get("mensagem")!=None else ":"
+        mensagem =mensagem if mensagem[-1]==":" else mensagem+":"
         while True: 
             try:
                 interacao=input(mensagem)
                 interacao=kwargs["tipo"](interacao)
                 return interacao
             except:
-                ...
+                if kwargs.get("erro") != None:
+                    print(kwargs["erro"])
+                else:
+                    print("ERRO")
     else:
         return None
+        
     
